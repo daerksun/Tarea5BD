b72513d88cfd2c3d7fea54b235d70155193ba7fb
sdist <sdist@itam.mx>
2017-03-22 16:57:07 -0600

    avanze

--- WindowsFormsApplication1/WindowsFormsApplication1/FrmCadena.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/FrmCadena.cs
@@ -21,6 +21,7 @@ namespace WindowsFormsApplication1
         public FrmCadena()
         {
             InitializeComponent();
+            dtFecha.CustomFormat = "dd-MM-yy";
         }
 
         private void FrmCadena_Load(object sender, EventArgs e)
@@ -29,6 +30,7 @@ namespace WindowsFormsApplication1
             cadSql = "SELECT nombreC FROM T4cliente";
             GestorBD.consBD(cadSql, "Client", dsClientes);
             com.cargaCombo(cbClientes , dsClientes, "Client", "nombreC");
+          
         }
 
         private void cbClientes_SelectedIndexChanged(object sender, EventArgs e)
@@ -39,25 +41,46 @@ namespace WindowsFormsApplication1
 
         private void btConsulta_Click(object sender, EventArgs e)
         {
-            //int idCl = idCliente(cbClientes.SelectedText);
-            MessageBox.Show(dtFecha.Text);
+            int idCl = idCliente(cbClientes.SelectedItem.ToString());
+            int montot, numPag, montp, saldo;
             GestorBD = new GestorBDT.GestorBD("MSDAORA", "bd16", "tregui", "oracle");
             cadSql = "SELECT f.fechaCompra, pro.nombreP FROM T4producto pro, T4fact_prod fp, T4factura f WHERE "+ 
                      "pro.idproducto = fp.idproducto AND fp.folio = f.folio AND "+
-                     "f.idCliente = 'Raul Orozco' AND f.fechaCompra >= '" + dtFecha.Text + "'";
+                     "f.idCliente = "+ idCl +" AND f.fechaCompra >= '" + dtFecha.Text + "'";
             GestorBD.consBD(cadSql, "Compras", dsCompras);
             dataGridView1.DataSource = dsCompras.Tables["Compras"];
+
+            cadSql = "SELECT sum(montototal) FROM T4factura WHERE idCliente = "+idCl;
+            montot = rellenaLabel(lbMontoTotal, cadSql);
+
+            //  cadSql = "";
+            //dsCompras.Clear();
+            //GestorBD.consBD(cadSql, "", dsCompras);
+            //lbNumPagos.Text = 
+
+            //  cadSql = "";
+            //dsCompras.Clear();
+            //GestorBD.consBD(cadSql, "", dsCompras);
+            //lbMontoPagos.Text = 
+
+            //  cadSql = "";
+            //dsCompras.Clear();
+            //GestorBD.consBD(cadSql, "", dsCompras);
+            //lbSaldoAct.Text = 
         }
 
-        //private int idCliente(String Nom)
-        //{
-        //    DataRow fi;
-        //    int resp;
-        //    GestorBD = new GestorBDT.GestorBD("MSDAORA", "bd16", "tregui", "oracle");
-        //    cadSql = "";
-        //    GestorBD.consBD(cadSql, "id", dsCliente);
-        //    fi= dsCompras.Tables["id"].Rows[0];
+        private int rellenaLabel(Label lb, String cadsql) {
 
-        //}
+        }
+        private int idCliente(String Nom) {
+          DataRow fi;
+          int resp;
+          GestorBD = new GestorBDT.GestorBD("MSDAORA", "bd16", "tregui", "oracle");
+          cadSql = "SELECT idCliente from T4Cliente where nombreC = '"+ Nom + "'";
+          GestorBD.consBD(cadSql, "id", dsCliente);
+          fi = dsCliente.Tables["id"].Rows[0];
+          resp = int.Parse(fi[0].ToString());
+          return resp;
+        }
     }
 }
