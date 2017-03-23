bf39d9ef28b6017f961ec16207c7e819b807d508
sdist <sdist@itam.mx>
2017-03-22 21:53:43 -0600

    22/03/2017 10PM

--- WindowsFormsApplication1/WindowsFormsApplication1/FrmFunciones.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/FrmFunciones.cs
@@ -17,7 +17,7 @@ namespace WindowsFormsApplication1 {
 
     OleDbConnection cnOracle;
     GestorBDT.GestorBD GestorBD;
-    DataSet dsSucs = new DataSet();
+    DataSet dsGeneral = new DataSet();
     const int OK = 1;
     Comunes com = new Comunes();
     String cadSql;
@@ -25,14 +25,20 @@ namespace WindowsFormsApplication1 {
     private void funciones_Load(object sender, EventArgs e) {
       GestorBD = new GestorBDT.GestorBD("MSDAORA", "bd16", "tregui", "oracle");
       cadSql = "SELECT nombreSuc FROM T4Sucursal";
-      GestorBD.consBD(cadSql, "Suc", dsSucs);
-      com.cargaCombo(cbSucursales, dsSucs, "Suc", "nombreSuc");
+      GestorBD.consBD(cadSql, "Suc", dsGeneral);
+      com.cargaCombo(cbSucursales, dsGeneral, "Suc", "nombreSuc");
+
+      cadSql = "SELECT nombreP FROM T4producto";
+      dsGeneral.Clear();
+      GestorBD.consBD(cadSql, "Prods", dsGeneral);
+      com.cargaCombo(cbProductos, dsGeneral, "Prods", "nombreP");
+
     }
 
+    //Ejecución de función
     private void btCons1_Click(object sender, EventArgs e) {
 
       if (cbSucursales.SelectedIndex > -1) {
-
         //Variables
         String nomSuc = cbSucursales.SelectedItem.ToString();
         String nomCad;
@@ -43,9 +49,9 @@ namespace WindowsFormsApplication1 {
 
         //Obtiene el nombre
         cadSql = "SELECT nombre FROM T4cadena cad, T4sucursal s WHERE cad.idcadena = s.idcadena AND s.nombreSuc = '" + nomSuc + "'";
-        dsSucs.Clear();
-        GestorBD.consBD(cadSql, "NomCad", dsSucs);
-        dr = dsSucs.Tables["NomCad"].Rows[0];
+        dsGeneral.Clear();
+        GestorBD.consBD(cadSql, "NomCad", dsGeneral);
+        dr = dsGeneral.Tables["NomCad"].Rows[0];
         nomCad = dr[0].ToString();
 
         //Conexión
@@ -79,11 +85,69 @@ namespace WindowsFormsApplication1 {
           MessageBox.Show(error.Message);
         }
         cnOracle.Close();
-      } else {
-        
-      }
+      } else
+        MessageBox.Show("No se seleccionó sucursal");       
+    }
 
-      
+    private void button1_Click(object sender, EventArgs e) {
+      this.Close();
+    }
+
+    //Ejecucíón de procedimiento almacenado
+    private void BTProc_Click(object sender, EventArgs e) {
+
+      if (cbSucursales.SelectedIndex > -1 || tbMonto.Text != "") {
+        String nomProd; double monto;
+        OleDbCommand cmd;
+        OleDbParameter param;
+
+        //Conexión a la BD.
+        cnOracle = new OleDbConnection("Provider=MSDAORA; Data Source=oracle;" +
+          "User ID=BD16;Password=tregui");
+        cnOracle.Open();
+        cmd = new OleDbCommand();
+        cmd.Connection = cnOracle;
+
+        //Llamado al procedimiento
+        cmd.CommandText = "T4NumEmpresas";
+        cmd.CommandType = CommandType.StoredProcedure;
+
+        //Parámetros
+        //Entrada
+        nomProd = cbProductos.SelectedItem.ToString();
+        monto = Double.Parse(tbMonto.Text);
+        param = new OleDbParameter("nomP", nomProd);
+        cmd.Parameters.Add(param);
+        param = new OleDbParameter("monto", monto);
+        cmd.Parameters.Add(param);
+
+        //Salida
+        param = new OleDbParameter("nombre", OleDbType.VarChar);
+        cmd.Parameters.Add(param);
+        param = new OleDbParameter("cant", OleDbType.Double,
+          8, ParameterDirection.Output, false, 4, 0, "cantidad", DataRowVersion.Current, 0);
+        cmd.Parameters.Add(param);
+
+
+        //Procedimiento
+        try {
+          cmd.ExecuteNonQuery();
+          int cant;
+          String cadena;
+          //Recuperar valores
+          cant = Convert.ToInt16(cmd.Parameters["monto"].Value);
+          cadena = cmd.Parameters["nombre"].Value.ToString();
+          MessageBox.Show("Cantidad de cadenas con " + nomProd + " menor a " + monto.ToString() + ": " + cant +
+            "\nSe puede encontrar en: " + cadena);
+
+        } catch (OleDbException error) {
+          MessageBox.Show(error.Message);
+        }
+
+        cnOracle.Close();
+      }
+      else
+        MessageBox.Show("Faltan datos"); 
     }
   }
 }
